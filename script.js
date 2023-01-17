72f52f577c04d8a97ebd27c20816dca87ea95b17
E-Albert <magicspoon123@gmail.com>
2023-01-16 19:53:12 -0500

    added a function to handle form submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let homeButton = document.getElementById("homeButton");
-let homePage = document.getElementById("homePage")
+let homePage = document.getElementById("startPage");
+let formInfo = document.getElementById("formContext");
+let cityInput = document.getElementById("cityInput");
+
 
 homeButton.addEventListener("click", pageGone)
 
@@ -7,3 +10,14 @@ function pageGone() {
     console.log('the button was clicked')
     homePage.classList.add("hide")
 }
+
+function formSubmitHandler(e) {
+    e.preventDefault()
+    console.log(e)
+    console.log("You're doing great keep on going!")
+
+    let city = cityInput.value.trim();
+    console.log(city)
+}
+
+formInfo.addEventListener("submit", formSubmitHandler)
