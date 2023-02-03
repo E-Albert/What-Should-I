94764af3b9a2429c1020d42a03f9f4557d97220b
E-Albert <magicspoon123@gmail.com>
2023-02-03 11:19:00 -0500

    able to show results from findPlaces function

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ let header = document.getElementById("header");
 let main = document.getElementById("main");
 let footer = document.getElementById("footer");
 
-
 let apiKey = "e681224f251edf9fe2b18dfc26040eac";
-let mapKey = "AIzaSyB2jsY4UMem8T06ilsqSs9W4YcS6IyCZac"
+let mapKey = "AIzaSyB2jsY4UMem8T06ilsqSs9W4YcS6IyCZac";
+
 
 homeButton.addEventListener("click", hideNSeek);
 
 function hideNSeek() {
   console.log("the button was clicked");
-    homePage.classList.add("hide");
-    header.classList.remove("hide");
-    main.classList.remove("hide");
-    footer.classList.remove("hide");
+  homePage.classList.add("hide");
+  header.classList.remove("hide");
+  main.classList.remove("hide");
+  footer.classList.remove("hide");
 }
 
 function formSubmitHandler(e) {
@@ -26,8 +26,11 @@ function formSubmitHandler(e) {
   console.log("The form was submitted! You're doing great keep on going!");
 
   let city = cityInput.value.trim();
+  // let activity = activityInput.value.trim()
   console.log(`Entered city: ${city}`);
+  // console.log(`Entered activity: ${activity}`)
   grabCityCords(city);
+  // findPlaces(activity)
   cityInput.value = "";
 }
 
@@ -40,11 +43,13 @@ function grabCityCords(city) {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      let latitude = parseFloat(data[0].lat);
-      let longitude = parseFloat(data[0].lon);
+      let latitude = parseFloat(data[0].lat.toFixed(5));
+      let longitude = parseFloat(data[0].lon.toFixed(5));
 
-      console.log(typeof latitude)
-      console.log(typeof longitude)
+      console.log(latitude)
+      console.log(longitude)
+      console.log(typeof latitude);
+      console.log(typeof longitude);
 
       console.log(`The latitude of ${city} is: ${latitude}`);
       console.log(`The longitude of ${city} is: ${longitude}`);
@@ -63,35 +68,55 @@ function getWeather(latitude, longitude) {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      let temp = data.main.temp
-      console.log(`The temperature is ${temp} degrees farenheit`)
+      let temp = data.main.temp;
+      console.log(`The temperature is ${temp} degrees farenheit`);
     })
     .catch((err) => console.log(err));
 }
 
-
-
-
 // Initialize and add the map
 function initMap(latitude, longitude) {
-
   // The map centered at user city
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
-    center: {lat: latitude, lng: longitude}
+    center: { lat: latitude, lng: longitude },
   });
-  
 }
 
 function findPlaces(latitude, longitude) {
+  
+  let activity = activityInput.value.trim()
 
-  let placesApi = `https://floating-headland-95050.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${mapKey}&location=${latitude},${longitude}&radius=50000&keyword=${activityInput}`
+  console.log(latitude)
+  console.log(longitude)
+  console.log(activity)
+  console.log(mapKey)
 
+  let placesApi = `https://floating-headland-95050.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${mapKey}&location=${latitude},${longitude}&radius=50000&type=${activity}`;
+  
   fetch(placesApi)
     .then((res) => res.json())
-    .then((data) => { console.log(data) })
-    .catch(err => console.log(err))
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
 }
+
+// function googleGeoCode(city) {
+//   console.log(city)
+//   let googleGeoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${mapKey}`
+
+//   fetch(googleGeoUrl)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data)
+//       console.log(data.results[0].geometry.location.lat)
+//       let googleGeoLat = parseFloat(data.results[0].geometry.location.lat)
+//       let googleGeoLon = parseFloat(data.results[0].geometry.location.lon)
+//       initMap(googleGeoLat, googleGeoLon)
+//     })
+//     .catch(err => console.log(err))
+// }
 
 window.initMap = initMap;
 
